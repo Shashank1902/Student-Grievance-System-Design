d11cadac7a8736a876c55397466370bdc4f941d2
Shashank Vishwakarma <shashank1902aman@gmail.com>
2021-09-19 02:16:13 +0530

    refactored HomeContent

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import React from 'react';
-import './App.css';
-<<<<<<< HEAD
-import Chatbot from './Components/Chat bot/Chatbot';
-import Grievanceform from './Components/GrievanceForm/Grievanceform';
-=======
-import HomeContent from './Components/Home content/Homecontent';
- import Chatbot from './Components/Chat bot/Chatbot';
-import Categorysection from './Components/CategorySection/Categorysection';
->>>>>>> aa690e124438b982be337f261fbb935a0dc0146c
+import React from "react";
+import "./App.css";
+import Chatbot from "./Components/Chat bot/Chatbot";
+import Grievanceform from "./Components/GrievanceForm/Grievanceform";
+import HomeContent from "./Components/Home content/Homecontent";
+import Categorysection from "./Components/CategorySection/Categorysection";
 
- class App extends React.Component {
+class App extends React.Component {
   render() {
-  return (
-    <div className="App">
-<<<<<<< HEAD
-      <Grievanceform/>
-      <Chatbot/>
-=======
-   <HomeContent/>
-    
-    <Chatbot/>
-    <Categorysection />
->>>>>>> aa690e124438b982be337f261fbb935a0dc0146c
-    </div>
-    
-  );
+    return (
+      <div className="App">
+        <HomeContent />
+        <Grievanceform />
+        <Chatbot />
+        <Categorysection />
+      </div>
+    );
   }
 }
 
--- src/Components/Home content/Homecontent.js
+++ src/Components/Home content/Homecontent.js
@@ -1,16 +1,26 @@
-import "./homecontent.css"
+import "./homecontent.css";
 function HomeContent() {
-    return (
-        <>
-            <div className="homecontent-txt">
-                <p classname="homecontent">This Web Application is developed to provide students with a platform where an easy, user-friendly interface serves the purpose of lodging complaints in a more formal pattern. </p>
-                <p classname="homecontent">It allows users to lay forward their grievances belonging to any of our specified categories of academic and/or non-academic issues. The college admin would look into all the lodged complaints and solve the issues accordingly in the best interest of its students.</p>
-               <div><image className="homeimage"
-                           src="assets/student-home-img.png"/></div>
-            </div>
-
-        </>
-
-    )
+  return (
+    <>
+      <div className="homecontent">
+        <div className="homecontent-txt-container">
+          <p classname="homecontent-txt">
+            This Web Application is developed to provide students with a
+            platform where an easy, user-friendly interface serves the purpose
+            of lodging complaints in a more formal pattern.{" "}
+          </p>
+          <p classname="homecontent-txt">
+            It allows users to lay forward their grievances belonging to any of
+            our specified categories of academic and/or non-academic issues. The
+            college admin would look into all the lodged complaints and solve
+            the issues accordingly in the best interest of its students.
+          </p>
+        </div>
+        <div className="homeImage-container">
+          <img className="homeImage" src="assets/images/student-home-img.png" />
+        </div>
+      </div>
+    </>
+  );
 }
 export default HomeContent;
