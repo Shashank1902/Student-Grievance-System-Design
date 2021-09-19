42d6b68df52192f156baa5571cc4acf602cbcd0e
Shruti Choudhary <shrutic29401@gmail.com>
2021-09-19 12:01:25 +0530

    navbar pushed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Chatbot from "./Components/Chat bot/Chatbot";
 import Grievanceform from "./Components/GrievanceForm/Grievanceform";
 import HomeContent from "./Components/Home content/Homecontent";
 import Categorysection from "./Components/CategorySection/Categorysection";
+import TopNavbar from "./Components/TopNavbar/TopNavbar";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <TopNavbar />
         <HomeContent />
         <Grievanceform />
         <Chatbot />
