af53538183b16e15eff4251d0a4ba43faa2c859b
Shruti Choudhary <shrutic29401@gmail.com>
2021-09-29 20:21:29 +0530

    pull request

--- src/Pages/StudentCommunity.jsx
+++ src/Pages/StudentCommunity.jsx
@@ -5,14 +5,11 @@ import Chatbot from "../Components/Chat bot/Chatbot";
 //import Categorysection from "../Components/CategorySection/Categorysection";
 import Footbar from "../Components/Footbar/Footbar";
 import TopNavbar from "../Components/TopNavbar/TopNavbar";
-<<<<<<< HEAD
 // import Categorysection from "../Components/CategorySection/Categorysection";
 import Post from "../Components/Post/Post";
 
-=======
 import Categorysection from "../Components/CategorySection/Categorysection";
 import UploadPost from "../Components/UploadPost/UploadPost";
->>>>>>> 54f3c1d8d229414dfa28c170109c60a9f305d827
 
 
 class StudentCommunity extends React.Component {
