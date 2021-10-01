8b7f80789b2f9438069a8d8df3198ba7f4a0d28a
Shashank Vishwakarma <shashank1902aman@gmail.com>
2021-10-01 19:48:09 +0530

    login page update

--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,43 +1,70 @@
-import React from 'react';
-import './login.css';
+import React, { useRef } from "react";
+import "./login.css";
+import axios from "axios";
 
 const Login = () => {
-    
-    
-    
-    return (
-        <div>
-            <div className="login-container">
 
-                <div className="login-box">
-                    <div className="login-heading-box">
-                        <h2 className="login-heading" >Login</h2>
+    const email = useRef("null");
+    const password = useRef("null");
 
-                    </div>
-                    <form >
-                        <div className="inputbox">
-                            <input type="text" name="username" required="" placeholder= "Username"/ >
-                           
-                        </div>
-                        <div className="inputbox">
-                            <input type="password" name="password" required="" placeholder= "Password"/>
-                            
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-                        </div>
+        console.log(email.current.value, password.current.value);
+        const userEmail = email.current.value;
+        const userPassword = password.current.value;
+        axios.post("http://localhost:8000/login", {email: userEmail, password: userPassword})
+        .then(response => {
+            console.log(response);
+        });
+    }
 
-                        <div className="login-btn-box">
-                            <button className="login-btn" type="submit">Submit</button>
-                        </div>
-
-                        <div className="signup-btn-box">
-                            <span>Register Here <button className="signup-btn" type="submit">Sign in</button> </span>
-                        </div>
-                        
-                    </form>
-                </div>
+  return (
+    <div>
+      <div className="login-container">
+        <div className="login-box">
+          <div className="login-heading-box">
+            <h2 className="login-heading">Login</h2>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="inputbox">
+              <input
+                type="text"
+                name="username"
+                required=""
+                placeholder="Username"
+                ref={email}
+              />
             </div>
+            <div className="inputbox">
+              <input
+                type="password"
+                name="password"
+                required=""
+                placeholder="Password"
+                ref={password}
+              />
+            </div>
+
+            <div className="login-btn-box">
+              <button className="login-btn" type="submit">
+                Submit
+              </button>
+            </div>
+
+            <div className="signup-btn-box">
+              <span>
+                Register Here{" "}
+                <button className="signup-btn" type="submit">
+                  Sign up
+                </button>{" "}
+              </span>
+            </div>
+          </form>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
