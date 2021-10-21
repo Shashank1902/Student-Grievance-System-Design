25ae7de9e3314ba30d7286000b89775f7bc2567d
Shruti Choudhary <shrutic29401@gmail.com>
2021-10-21 13:28:14 +0530

    footbar links

--- src/Components/Footbar/Footbar.jsx
+++ src/Components/Footbar/Footbar.jsx
@@ -1,13 +1,20 @@
 import "./footbar.css";
+import { Link } from "react-router-dom";
 
 function Footbar() {
   return (
     <>
       <div className="foot-container">
         <div className="foot">
-          <p>Student Community</p>
-          <p>Profile</p>
-          <p>Help</p>
+          <Link style={{ textDecoration: "none", color: "white"}} to="/StudentCommunity">
+            <p>Student Community</p>
+          </Link>
+          <Link style={{ textDecoration: "none", color: "white" }} to="/Profile">
+            <p>Profile</p>
+          </Link>
+          <Link style={{ textDecoration: "none", color: "white" }} to="/AboutUs">
+            <p>About Us</p>
+          </Link>
         </div>
         <div className="foot">
           <p>Contact Developer</p>
@@ -15,9 +22,9 @@ function Footbar() {
           <p>Feedback</p>
         </div>
         <div className="footbars">
-            <p>
-                Designed and developed by team &lt;em&gt;brace, CSE 2019-23
-            </p>
+          <p>
+            Designed and developed by team &lt;em&gt;brace, CSE 2019-23
+          </p>
         </div>
       </div>
     </>
